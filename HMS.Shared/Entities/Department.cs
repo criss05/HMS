467a89a0adaab98298ff95878efe03d67069a35a
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-05-31 21:14:24 +0300

    add Department to class library add Doctor to class library

---
+++ HMS.Shared/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Entities
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        // navigation property for related doctors
+        public ICollection<Doctor> Doctors { get; set; } = new List<Doctor>();
+    }
+}
---
+++ HMS.Shared/Entities/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Entities
+{
+    public class Doctor : User
+    {
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [ForeignKey("DepartmentId")]
+        public Department Department { get; set; } = null!;
+
+        [Required]
+        public int YearsOfExperience { get; set; }
+
+        [Required]
+        public string LicenseNumber { get; set; } = "";
+    }
+}
