73d30dae9edd9ca14b14f65b43bc8c8433edb9f3
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-05-31 21:05:18 +0300

    add adming class to model

---
+++ HMS.Shared/Entities/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Entities
+{
+    [Table("Admins")]
+    public class Admin : User
+    {
+        // for now this has no admin-specific fields..
+    }
+}
