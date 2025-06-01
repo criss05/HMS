afa5bca781070c9218e71b16ce7d316e6d12b8ed
Robert076 <77006934+Robert076@users.noreply.github.com>
2025-06-01 17:51:15 +0300

    comments for iuserrepository

--- HMS.Backend/Repositories/Interfaces/IUserRepository.cs
+++ HMS.Backend/Repositories/Interfaces/IUserRepository.cs
@@ -4,12 +4,43 @@ using HMS.Shared.Entities;
 
 namespace HMS.Backend.Repositories.Interfaces
 {
+    /// <summary>
+    /// Repository interface for managing User entities.
+    /// </summary>
     public interface IUserRepository
     {
+        /// <summary>
+        /// Retrieves all users asynchronously.
+        /// </summary>
+        /// <returns>A collection of all users.</returns>
         Task<IEnumerable<User>> GetAllAsync();
+
+        /// <summary>
+        /// Retrieves a user by their unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the user.</param>
+        /// <returns>The user if found; otherwise, null.</returns>
         Task<User?> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Adds a new user asynchronously.
+        /// </summary>
+        /// <param name="user">The user to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task AddAsync(User user);
+
+        /// <summary>
+        /// Updates an existing user asynchronously.
+        /// </summary>
+        /// <param name="user">The user with updated information.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task UpdateAsync(User user);
+
+        /// <summary>
+        /// Deletes a user by their unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the user to delete.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task DeleteAsync(int id);
     }
 }
