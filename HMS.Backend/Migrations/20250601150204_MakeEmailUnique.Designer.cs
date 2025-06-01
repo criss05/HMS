1e0b8179e182f21b650f55f47c15d1bd0d70cde3
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2025-06-01 18:06:31 +0300

    fix unique email

--- HMS.Backend/Migrations/20250601150204_MakeEmailUnique.Designer.cs
+++ HMS.Backend/Migrations/20250601150204_MakeEmailUnique.Designer.cs
@@ -4,6 +4,7 @@ using HMS.Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HMS.Backend.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250601150204_MakeEmailUnique")]
+    partial class MakeEmailUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
