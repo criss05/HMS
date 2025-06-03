59f8c3bbf9eedef5c2cd452d1f9e01b5c3fe8054
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-06-04 02:37:11 +0300

    create the doctor repo interface in share

---
+++ HMS.Shared/Repositories/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,46 @@
+﻿using HMS.Shared.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Repositories.Interfaces
+{
+    /// <summary>
+    /// Interface for managing Doctor entities in the data store.
+    /// </summary>
+    public interface IDoctorRepository
+    {
+        /// <summary>
+        /// Gets all doctors asynchronously.
+        /// </summary>
+        /// <returns>A collection of all doctors.</returns>
+        Task<IEnumerable<Doctor>> GetAllAsync();
+
+        /// <summary>
+        /// Gets a doctor by their unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the doctor.</param>
+        /// <returns>The doctor if found, otherwise null.</returns>
+        Task<Doctor?> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Adds a new doctor asynchronously.
+        /// </summary>
+        /// <param name="doctor">The doctor to add.</param>
+        /// <returns>The added doctor.</returns>
+        Task<Doctor> AddAsync(Doctor doctor);
+
+        /// <summary>
+        /// Updates an existing doctor asynchronously.
+        /// </summary>
+        /// <param name="doctor">The doctor with updated data.</param>
+        /// <returns>True if update succeeded, false otherwise.</returns>
+        Task<bool> UpdateAsync(Doctor doctor);
+
+        /// <summary>
+        /// Deletes a doctor by their unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the doctor to delete.</param>
+        /// <returns>True if deletion succeeded, false otherwise.</returns>
+        Task<bool> DeleteAsync(int id);
+    }
+}
