f000f871c8238b4678179d66c905445345292cdb
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-06-04 02:38:49 +0300

    create the user repo interface in share

---
+++ HMS.Shared/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HMS.Shared.Entities;
+
+namespace HMS.Shared.Repositories.Interfaces
+{
+    /// <summary>
+    /// Repository interface for managing User entities.
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Retrieves all users asynchronously.
+        /// </summary>
+        /// <returns>A collection of all users.</returns>
+        Task<IEnumerable<User>> GetAllAsync();
+
+        /// <summary>
+        /// Retrieves a user by their unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the user.</param>
+        /// <returns>The user if found; otherwise, null.</returns>
+        Task<User?> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Retrieves a user by their email address asynchronously.
+        /// </summary>
+        /// <param name="email">The email address of the user.</param>
+        /// <returns>The user if found; otherwise, null.</returns>
+        Task<User?> GetByEmailAsync(string email);
+
+        /// <summary>
+        /// Adds a new user asynchronously.
+        /// </summary>
+        /// <param name="user">The user to add.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task AddAsync(User user);
+
+        /// <summary>
+        /// Updates an existing user asynchronously.
+        /// </summary>
+        /// <param name="user">The user with updated information.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task UpdateAsync(User user);
+
+        /// <summary>
+        /// Deletes a user by their unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the user to delete.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task DeleteAsync(int id);
+    }
+}
