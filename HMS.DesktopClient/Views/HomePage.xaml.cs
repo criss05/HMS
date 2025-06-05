db5f7587fd64838e77760d12c556680b4c51cdaf
Silea Horia <horia.silea@gmail.com>
2025-06-05 20:00:54 +0300

    wtf

--- HMS.DesktopClient/Views/HomePage.xaml.cs
+++ HMS.DesktopClient/Views/HomePage.xaml.cs
@@ -12,12 +12,6 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using HMS.Shared.Proxies.Implementations;
-using System.Diagnostics;
-using Windows.Media.Capture.Core;
-using HMS.Shared.Entities;
-using HMS.Shared.DTOs;
-using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -32,16 +26,7 @@ namespace HMS.DesktopClient.Views
         public HomePage()
         {
             this.InitializeComponent();
-            DepartmentProxy dp = new(App.CurrentUser.Token);
             RootGrid.DataContext = App.CurrentUser;
-            Debug.WriteLine("afjdbgfadjgldsjglkadsg");
-            test(dp);
-        }
-
-        public async void test(DepartmentProxy dp)
-        {
-            DepartmentDto dto = await dp.GetByIdAsync(1);
-            Debug.WriteLine(dto.Name);
         }
     }
 }
