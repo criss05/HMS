7646b3b030de67a412a1116abf9592c540f87536
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-06-06 23:52:24 +0300

    Add NotificationService for user notifications handling

---
+++ HMS.Shared/Services/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using HMS.Shared.DTOs;
+using HMS.Shared.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Services
+{
+    public class NotificationService
+    {
+        private INotificationRepository _notificationRepository;
+
+        public NotificationService(INotificationRepository notificationRepository)
+        {
+            this._notificationRepository = notificationRepository;
+        }
+
+        public async Task<IEnumerable<NotificationDto>> GetNotificationsByUserIdAsync(int userId)
+        {
+            try
+            {
+                return await _notificationRepository.GetNotificationsByUserIdAsync(userId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve notifications: " + ex.Message);
+            }
+        }
+    }
+}
