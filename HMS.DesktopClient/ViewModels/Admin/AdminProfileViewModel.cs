69fe2889769c6248c296f23894da3c28cdbbce31
paulb <paulberca@gmail.com>
2025-06-06 19:45:54 +0300

    stylecop documentation

--- HMS.DesktopClient/ViewModels/Admin/AdminProfileViewModel.cs
+++ HMS.DesktopClient/ViewModels/Admin/AdminProfileViewModel.cs
@@ -14,14 +14,25 @@ using HMS.Shared.Entities;
 
 namespace HMS.DesktopClient.ViewModels
 {
+    /// <summary>
+    /// View model for the admin profile screen that provides admin information and editing capabilities.
+    /// </summary>
     public class AdminProfileViewModel : INotifyPropertyChanged
     {
         private readonly UserWithTokenDto _user;  // holds token and user info
         private AdminDto _admin;                  // holds admin-specific + user info (except token)
         private readonly AdminService _adminService;
 
+        /// <summary>
+        /// Event that is fired when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdminProfileViewModel"/> class.
+        /// </summary>
+        /// <param name="user">The authenticated user with token.</param>
+        /// <param name="admin">The admin data to display and edit.</param>
         public AdminProfileViewModel(UserWithTokenDto user, AdminDto admin)
         {
             _user = user;
@@ -30,6 +41,9 @@ namespace HMS.DesktopClient.ViewModels
             _adminService = new AdminService(proxy);
         }
 
+        /// <summary>
+        /// Gets or sets the name of the admin.
+        /// </summary>
         public string Name
         {
             get => _admin.Name ?? "";
@@ -43,6 +57,9 @@ namespace HMS.DesktopClient.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets or sets the phone number of the admin.
+        /// </summary>
         public string PhoneNumber
         {
             get => _admin.PhoneNumber ?? "";
@@ -56,6 +73,9 @@ namespace HMS.DesktopClient.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets or sets the email address of the admin.
+        /// </summary>
         public string Email
         {
             get => _admin.Email ?? "";
@@ -69,6 +89,9 @@ namespace HMS.DesktopClient.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets or sets the CNP (Personal Numeric Code) of the admin.
+        /// </summary>
         public string CNP
         {
             get => _admin.CNP ?? "";
@@ -82,14 +105,31 @@ namespace HMS.DesktopClient.ViewModels
             }
         }
 
+        /// <summary>
+        /// Gets the ID of the admin.
+        /// </summary>
         public int Id => _admin.Id;
+
+        /// <summary>
+        /// Gets the role of the admin.
+        /// </summary>
         public string Role => _admin.Role.ToString();
+
+        /// <summary>
+        /// Gets the creation date and time of the admin account.
+        /// </summary>
         public string CreatedAt => _admin.CreatedAt.ToString("yyyy-MM-dd HH:mm");
 
 
+        /// <summary>
+        /// Gets the token used for authenticating the admin.
+        /// </summary>
         public string Token => _user.Token;
 
         private string _errorMessage;
+        /// <summary>
+        /// Gets or sets the error message to display to the user.
+        /// </summary>
         public string ErrorMessage
         {
             get => _errorMessage;
@@ -103,6 +143,12 @@ namespace HMS.DesktopClient.ViewModels
             }
         }
 
+        /// <summary>
+        /// Loads admin data asynchronously from the service by admin ID.
+        /// </summary>
+        /// <param name="adminId">The ID of the admin to load.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="Exception">Thrown when the admin is not found.</exception>
         public async Task LoadAdminAsync(int adminId)
         {
             var admin = await _adminService.GetAdminByIdAsync(adminId);
@@ -118,6 +164,11 @@ namespace HMS.DesktopClient.ViewModels
             OnPropertyChanged(nameof(Role));
             OnPropertyChanged(nameof(CreatedAt));
         }
+
+        /// <summary>
+        /// Raises the PropertyChanged event for the specified property.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed.</param>
         private void OnPropertyChanged(string propertyName) =>
              PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
